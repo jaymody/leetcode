5c718ec71442765e6070f3a0b6e17e136b32dc2f
Jay Mody <jaykmody@gmail.com>
2019-07-17 17:18:34 -0400

    faster solution (time complexity remains same however)

--- c++/11 - Container With Most Water.cpp
+++ c++/11 - Container With Most Water.cpp
@@ -12,18 +12,31 @@ public:
     int maxArea(vector<int>& height) {
         
         int max = 0;
+
         int left = 0;
         int right = height.size()-1;
+
+        int maxLeft = left;
+        int maxRight = right;
         
         while (left < right) {
             int area = (right - left)*(height[left] < height[right] ? height[left] : height[right]);
-            max = area > max ? area : max;
+            if (area > max) {
+                max = area;
+                maxLeft = left;
+                maxRight = right;
+            }
+
             
             if (height[left] < height[right]) {
-                left++;
+                do {
+                    left++;
+                } while (left < right && height[maxLeft] >= height[left]);
             }
             else {
-                right--;
+                do {
+                    right--;
+                } while (left < right && height[maxRight] >= height[right]);
             }
         }
         
