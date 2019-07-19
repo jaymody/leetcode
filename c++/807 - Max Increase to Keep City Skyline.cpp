1b23efa1bafb5d524795c1d5093b9d10c8842fd6
Jay Mody <jaykmody@gmail.com>
2019-07-19 14:28:51 -0400

    solution: 807 - max increase city skyline - c++

---
+++ c++/807 - Max Increase to Keep City Skyline.cpp
@@ -0,0 +1,52 @@
+// 807
+// Max Increase to Keep City Skyline
+// https://leetcode.com/problems/max-increase-to-keep-city-skyline/
+// c++
+// medium
+// O(m*n)
+// O(m+n)
+// basic
+
+class Solution {
+public:
+    int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        // Variables
+        int sum = 0;
+        
+        int nRows = grid.size();
+        int nCols = grid[0].size();
+        
+        vector<int> rowMax(nRows, -1);
+        vector<int> colMax(nCols, -1);
+        
+        // Get max for each row
+        for (int row = 0; row < nRows; row++) {
+            for (int col = 0; col < nCols; col++) {
+                if (grid[row][col] > rowMax[row])
+                    rowMax[row] = grid[row][col];
+            } 
+        }
+        // Get max for each col
+        for (int col = 0; col < nCols; col++) {
+            for (int row = 0; row < nRows; row++) {
+                if (grid[row][col] > colMax[col])
+                    colMax[col] = grid[row][col];
+            } 
+        }
+        
+        for (int row = 0; row < nRows; row++) {
+            for (int col = 0; col < nCols; col++) {
+                if (rowMax[row] < colMax[col]) {
+                    sum += rowMax[row] - grid[row][col];
+                    grid[row][col] = rowMax[row];
+                }
+                else {
+                    sum += colMax[col] - grid[row][col];
+                    grid[row][col] = colMax[col];
+                }
+            }
+        }
+        
+        return sum;
+    }
+};
