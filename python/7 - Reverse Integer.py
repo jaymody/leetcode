9d80ab7e93c24ae91e234b242436703e160c993f
Jay Mody <jaykmody@gmail.com>
2019-07-19 00:12:23 -0400

    7 reverse integer py

---
+++ python/7 - Reverse Integer.py
@@ -0,0 +1,33 @@
+# 7
+# Reverse Integer
+# https://leetcode.com/problems/reverse-integer/
+# python
+# easy
+# O(log n)
+# O(log n)
+# math string
+
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        s = str(x)
+        
+        if len(s)==1:
+            return x
+        
+        s=s[::-1]
+        
+        i = 0
+        while (s[i]=='0'):
+            i+=1
+        s = s[i:]
+        if s[-1]=='-':
+            s = ''.join(['-',s[:-1]])
+            
+        if abs(int(s)) > 2**31:
+            return 0;
+            
+        return s
