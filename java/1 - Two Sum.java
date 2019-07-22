3aee8fe50c7206a8e4c3dbb631e3b999cf9adfd7
Jay Mody <jaykmody@gmail.com>
2019-07-22 17:06:33 -0400

    warning about two sum brute force method

--- java/1 - Two Sum.java
+++ java/1 - Two Sum.java
@@ -7,7 +7,7 @@
 // O(1)
 // basic
 
-// O(n^2) time BRUTE FORCE METHOD VERY SLOW
+// O(n^2) time BRUTE FORCE METHOD VERY SLOW (see c++ or python implementation for O(n))
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length-1; i++) {
