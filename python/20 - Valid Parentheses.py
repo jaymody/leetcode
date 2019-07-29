1047c24493a2e70c9c769669c82693ae175504fd
Jay Mody <jaykmody@gmail.com>
2019-07-29 00:52:29 -0400

    solution: 20 - valid parentheses - c++ py

--- python/20 - Valid Parentheses.py
+++ python/20 - Valid Parentheses.py
@@ -3,6 +3,30 @@
 # https://leetcode.com/problems/valid-parentheses/
 # python
 # easy
-# 
-# 
+# O(n)
+# O(n)
 # string stack
+
+# Initial solution in python using python [O(n), O(n)]
+# Impoved version done in c++
+class Solution:
+    def isValid(self, s: str) -> bool:
+        d = {
+            "[": "]",
+            "{": "}",
+            "(": ")"
+        }
+        
+        stack = []
+        
+        for c in s:
+            if stack and stack[-1] == c:
+                del stack[-1]
+                continue
+                
+            if c in d.keys():
+                stack.append(d[c])
+            elif c in d.values():
+                return False    
+
+        return len(stack) == 0
