575e1644d324748f72e0da5d95fd0b32642ae699
Jay Mody <jaykmody@gmail.com>
2019-08-01 17:27:22 -0400

    added name of optimal solution

--- c++/287 - Find the Duplicate Number.cpp
+++ c++/287 - Find the Duplicate Number.cpp
@@ -11,6 +11,8 @@
 // and time complexity must be < O(n^2)
 
 // Initial solution using sort and linear search, O(n log n), O(1)
+// The solution can actually be produced using the Floyd's Tortoise and Hare
+// alogirithim (O(n) time, O(1) space)
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
