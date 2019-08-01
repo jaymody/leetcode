1db11d4918ef37e7db9559caeff29fde0474ef7f
Jay Mody <jaykmody@gmail.com>
2019-08-01 17:24:57 -0400

    solution: 287 - Find the Duplicate Number - c++

---
+++ c++/287 - Find the Duplicate Number.cpp
@@ -0,0 +1,27 @@
+// 287
+// Find the Duplicate Number
+// https://leetcode.com/problems/find-the-duplicate-number/
+// c++
+// array
+// 
+// 
+// two_pointers binary_search
+
+// Problem states space complexity must be O(1)
+// and time complexity must be < O(n^2)
+
+// Initial solution using sort and linear search, O(n log n), O(1)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i-1]) {
+                return nums[i];
+            }
+        }
+        
+        return -1;
+    }
+};
