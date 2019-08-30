08a48de0920f0ad83984713a3f836d6ebcba6aa4
Jay Mody <jaykmody@gmail.com>
2019-08-30 13:52:41 -0400

    update: 52 - c++ - slightly more efficient algoirthim using struct

--- c++/52 - N-Queens II.cpp
+++ c++/52 - N-Queens II.cpp
@@ -10,25 +10,32 @@
 // Quick, memory safe solution
 // time: O(n^n)
 // space: O(n)
+struct coord {
+    int r;
+    int c;
+};
+
 class Solution {
 public:
-    bool valid(int& r, int& c, vector<pair<int,int>>& vec) {
-        for (auto p : vec) {
-            if (p.second == c || abs(p.second-c) == abs(p.first-r))
+    bool valid(int& r, int& c, vector<coord>& vec) {
+        for (coord crd : vec) {
+            if (crd.c == c)
+                return false;
+            else if (abs(crd.c-c) == abs(crd.r-r))
                 return false;
         }
         return true;
     }
     
-    void visit(int& n, int r, vector<pair<int,int>> vec, int& count) {        
+    void visit(int& n, int r, vector<coord>& vec, int& count) {        
         for (int c = 0; c < n; c++) {
             if (!valid(r, c, vec))
                 continue;
             if (r == n-1)
                 count += 1;
             else {
-                pair p{r, c};
-                vec.push_back(p);
+                coord crd{r, c};
+                vec.push_back(crd);
                 visit(n, r+1, vec, count);
                 vec.pop_back();
             }
@@ -40,7 +47,7 @@ public:
             return 1;
         
         int count = 0;
-        vector<pair<int,int>> vec;
+        vector<coord> vec;
         visit(n, 0, vec, count);
         return count;
     }
