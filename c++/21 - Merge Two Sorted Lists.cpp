c6c0c92d317ea42f628f283d8ea7ee50d1020f88
Jay Mody <jaykmody@gmail.com>
2019-09-09 00:30:08 -0400

    added recursive solution

--- c++/21 - Merge Two Sorted Lists.cpp
+++ c++/21 - Merge Two Sorted Lists.cpp
@@ -16,7 +16,7 @@
  * };
  */
 
-// two pointer solution
+// two pointer iterative solution
 // time: O(n) where n is the length of the two lists
 // space: O(1)
 class Solution {
@@ -40,5 +40,25 @@ public:
         }
         
         return head->next;
+    }
+};
+
+// recursive solution
+// time: O(n)
+// space: O(1)
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        
+        if (l1->val <= l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } 
+        else {
+            l2->next = mergeTwoLists(l2->next, l1);
+            return l2;
+        }
     }
 };
