f98506b3b26048b4a85007f588875e2df8574325
Jay Mody <jaykmody@gmail.com>
2019-09-19 17:18:30 -0400

    solution: 102 - binary tree level order traversal - c++

---
+++ c++/102 - Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,44 @@
+// 102
+// Binary Tree Level Order Traversal
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+// c++
+// medium
+// 
+// 
+// tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// left to right recursive solution
+// time: O(n)
+// space: O(1)
+class Solution {
+public:    
+    void visit(TreeNode* root, vector<vector<int>>& result, int depth) {
+        if (root) {
+            if (depth == result.size()) {
+                vector<int> empty;
+                result.push_back(empty);
+            }
+
+            result[depth].push_back(root->val);
+
+            visit(root->left, result, depth+1);
+            visit(root->right, result, depth+1);
+        }
+    }
+    
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result; 
+        visit(root, result, 0);
+        return result;
+    }
+};
