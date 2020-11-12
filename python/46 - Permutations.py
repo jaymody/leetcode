c4c5b400f02c74d57afd7485e1a812390fa9530e
jaymody <jaykmody@gmail.com>
2020-11-12 15:53:21 -0500

    solution 46 - permutations - added additional algorithm

--- python/46 - Permutations.py
+++ python/46 - Permutations.py
@@ -19,10 +19,11 @@ class Solution:
                 results.append(path[:] + options[:])
             else:
                 for i in range(len(options)):
-                    recurse(path+[options[i]], options[:i] + options[i+1:])
-        
-        recurse([],nums)
+                    recurse(path + [options[i]], options[:i] + options[i + 1 :])
+
+        recurse([], nums)
         return results
+
 
 # initial swapping solution
 # time: O(n*n!)
@@ -31,14 +32,29 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         results = []
         end = len(nums)
-        
+
         def recurse(start):
             if start == end:
                 results.append(nums[:])
             for i in range(start, end):
                 nums[start], nums[i] = nums[i], nums[start]
-                recurse(start+1)
+                recurse(start + 1)
                 nums[start], nums[i] = nums[i], nums[start]
-        
+
         recurse(0)
         return results
+
+
+# another recursive algorithm
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+
+        def visit(curr, pool):
+            if not pool:
+                ans.append(curr)
+            for i in range(len(pool)):
+                visit(curr + [pool[i]], pool[:i] + pool[i + 1 :])
+
+        visit([], nums)
+        return ans
